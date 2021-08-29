26fb54d1d033a43045f65c5e12b2c220cf901c07
Josh Creek <jcreek93@gmail.com>
2021-08-29 15:41:15 +0100

    feat(*): Add YouTubeVideo model

---
+++ YouTubeChannelDownloader/Models/YouTubeVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode.Playlists;
+
+namespace YouTubeChannelDownloader.Models
+{
+    public class YouTubeVideo
+    {
+        public PlaylistVideo PlaylistVideo { get; set; }
+        public int VideoNumber { get; set; }
+    }
+}
