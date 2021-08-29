2e2d6771c09af71439392b8132b705be4dc11b8a
Josh Creek <jcreek93@gmail.com>
2021-08-29 15:40:22 +0100

    feat(*): Add db migrations

---
+++ YouTubeChannelDownloader/Migrations/20210826231651_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YouTubeChannelDownloader.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DownloadedVideos",
+                columns: table => new
+                {
+                    DownloadedVideoId = table.Column<string>(type: "TEXT", nullable: false),
+                    DownloadedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DownloadedVideos", x => x.DownloadedVideoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DownloadedVideos");
+        }
+    }
+}
