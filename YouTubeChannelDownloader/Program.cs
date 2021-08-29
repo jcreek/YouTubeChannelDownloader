f65701dbd9c747f05acd1146ebfea0dbb92009b9
Josh Creek <jcreek93@gmail.com>
2021-08-29 15:39:59 +0100

    feat(*): Add base project

---
+++ YouTubeChannelDownloader/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace YouTubeChannelDownloader
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+        }
+}
