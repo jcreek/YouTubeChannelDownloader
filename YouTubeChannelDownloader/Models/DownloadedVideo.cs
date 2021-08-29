4c725873dab1b6081396fe095437cb859fe9d809
Josh Creek <jcreek93@gmail.com>
2021-08-29 15:40:52 +0100

    feat(*): Add DownloadedVideo model

---
+++ YouTubeChannelDownloader/Models/DownloadedVideo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeChannelDownloader.Models
+{
+    public class DownloadedVideo
+    {
+        [Key]
+        public string DownloadedVideoId { get; set; }
+
+        public DateTime DownloadedDateTime { get; set; }
+        public string Title { get; set; }
+    }
+}
