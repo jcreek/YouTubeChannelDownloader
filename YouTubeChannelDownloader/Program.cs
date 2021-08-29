19958e1d169f83433a7712111331dbc66db1fb1b
Josh Creek <jcreek93@gmail.com>
2021-08-29 15:47:05 +0100

    feat(*): Add using statements

--- YouTubeChannelDownloader/Program.cs
+++ YouTubeChannelDownloader/Program.cs
@@ -1,7 +1,17 @@
+﻿using Creek.HelpfulExtensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YouTubeChannelDownloader.Data;
+using YouTubeChannelDownloader.Models;
+using YouTubeChannelDownloader.Services;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Playlists;
+using YoutubeExplode.Videos.Streams;
+using YouTubeChannelDownloader.Utilities;
 using System.IO;
 
 namespace YouTubeChannelDownloader
